3b4c1c3a498b3d6f0137489ce61753498478b482
Sebastian Böhme <sebastianboehme92@gmail.com>
2025-03-28 10:46:53 +0100

    imported navigation to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroSectionComponent } from '../app/hero-section/hero-section.component';
+import { NavigationComponent } from '../app/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroSectionComponent],
+  imports: [CommonModule, RouterOutlet, HeroSectionComponent, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
