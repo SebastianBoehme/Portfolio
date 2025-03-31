10d09fb87b0231f98e243c0d8245ae692c63dd92
Sebastian Böhme <sebastianboehme92@gmail.com>
2025-03-31 11:34:16 +0200

    implemented about me component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroSectionComponent } from '../app/hero-section/hero-section.component';
 import { NavigationComponent } from '../app/navigation/navigation.component';
+import { AboutMeComponent } from "../app/about-me/about-me.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroSectionComponent, NavigationComponent],
+  imports: [CommonModule, RouterOutlet, HeroSectionComponent, NavigationComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
