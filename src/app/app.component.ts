5bfdd746d446cfe0468e26ba24f0ab5370880bbd
Sebastian Böhme <sebastianboehme92@gmail.com>
2025-04-02 13:56:39 +0200

    implemented skills component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeroSectionComponent } from '../app/hero-section/hero-section.component';
 import { NavigationComponent } from '../app/navigation/navigation.component';
 import { AboutMeComponent } from "../app/about-me/about-me.component";
+import { SkillsComponent } from "./skills/skills.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroSectionComponent, NavigationComponent, AboutMeComponent],
+  imports: [CommonModule, RouterOutlet, HeroSectionComponent, NavigationComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
