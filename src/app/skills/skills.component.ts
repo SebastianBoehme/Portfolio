dba88c0a167338d86aa36bd7251e6961b5e62c56
Sebastian Böhme <sebastianboehme92@gmail.com>
2025-04-08 12:40:37 +0200

    addet an array for a later used forloop

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -9,4 +9,47 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
+  skills: { icon: string; name: string }[] = [
+    {
+      icon: '../../assets/img/skills-icons/html-icon.svg',
+      name: 'HTML'
+    },
+    {
+      icon: '../../assets/img/skills-icons/css-icon.svg',
+      name: 'CSS3'
+    },
+    {
+      icon: '../../assets/img/skills-icons/js-icon.svg',
+      name: 'JavaScript'
+    },
+    {
+      icon: '../../assets/img/skills-icons/material-design-icon.svg',
+      name: 'Material Design'
+    },
+    {
+      icon: '../../assets/img/skills-icons/ts-icon.svg',
+      name: 'TypeScript'
+    },
+    {
+      icon: '../../assets/img/skills-icons/angular-icon.svg',
+      name: 'Angular'
+    },
+    { icon: '../../assets/img/skills-icons/firebase-icon.svg',
+      name: 'Firebase'
+    },
+    {
+      icon: '../../assets/img/skills-icons/git-icon.svg',
+      name: 'GIT'
+    },
+    {
+      icon: '../../assets/img/skills-icons/rest-api--icon.svg',
+      name: 'Rest API'
+    },
+    {
+      icon: '../../assets/img/skills-icons/scrum-icon.svg',
+      name: 'Scrum'
+    }
+  ];
+
+
 }
