857331b680a0b86976d76f43ed11c85debb9df57
Sebastian Böhme <sebastianboehme92@gmail.com>
2025-06-29 12:38:00 +0200

    implented general design of the overlay

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -45,10 +45,10 @@ closeOverlay() {
       name: 'JOIN',
       description: 'Ein kollaboratives To-do-Tool nach dem Vorbild von Trello - Aufgaben erstellen, zuweisen und per Drag & Drop nach Status sortieren sowie Kontakte erstellen, bearbeiten und löschen.',
       tools: [
-        { name: 'HTML', icon: '' },
-        { name: 'CSS', icon: '' },
-        { name: 'JavaScript', icon: '' },
-        { name: 'Firebase', icon: '' }
+        { name: 'HTML', icon: '../../assets/img/projects-icons/html-icon-green.svg' },
+        { name: 'CSS', icon: '../../assets/img/projects-icons/css-icon-green.svg' },
+        { name: 'JavaScript', icon: '../../assets/img/projects-icons/javascript-icon-green.svg' },
+        { name: 'Firebase', icon: '../../assets/img/projects-icons/firebase-icon-green.svg' }
       ],
       short: 'JS',
       image: '../../assets/img/projects-icons/join-image.png',
@@ -59,9 +59,9 @@ closeOverlay() {
       name: 'El Pollo Loco',
       description: 'Ein kleines, lustiges 2D Spiel mit einem Objektorientierten Ansatz. Begleite Pepe bei seinem Kampf gegen einen Hühneraufstand. Renne, springe und bewirf die verrückten Hennen mit Tabascoflaschen',
       tools: [
-        { name: 'HTML', icon: 'assets/icons/angular.svg' },
-        { name: 'CSS', icon: 'assets/icons/scss.svg' },
-        { name: 'JavaScript', icon: 'assets/icons/typescript.svg' },
+        { name: 'HTML', icon: '../../assets/img/projects-icons/html-icon-green.svg' },
+        { name: 'CSS', icon: '../../assets/img/projects-icons/css-icon-green.svg' },
+        { name: 'JavaScript', icon: '../../assets/img/projects-icons/javascript-icon-green.svg' },
       ],
       short: 'JS',
       image: '../../assets/img/projects-icons/el-pollo-loco-image.png',
